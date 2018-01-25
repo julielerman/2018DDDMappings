f6192a1543d69f1c7990ffcb42f04838ce0616dc
julie <julielerman@gmail.com>
2018-01-25 17:36:52 -0500

    more tests and some tweaks to team

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Drawing;
 using System.Linq;
 using Xunit;
 
 namespace test {
     public class DomainClassTests {
-        
+
         private static Team CreateTeamAjax () {
             return new Team ("AFC Ajax", "The Lancers", "1900", "Amsterdam Arena");
         }
@@ -13,39 +14,71 @@ namespace test {
         public void NewTeamGetsId () {
             var team = CreateTeamAjax ();
             Assert.NotEqual (Guid.Empty, team.Id);
-       }
+        }
 
         [Fact]
         public void TeamAllowsNewPlayer () {
             var team = CreateTeamAjax ();
-            team.AddPlayer("André","Onana");
-            Assert.Equal("André Onana", team.Players.First().Name.FullName);
+            string response = "";
+            var playerWasAdded = team.AddPlayer ("André", "Onana", out response);
+            Assert.Equal ("André Onana", team.Players.First ().Name.FullName);
 
         }
-  [Fact]
+
+        [Fact]
         public void TeamAllowsMultiplePlayers () {
             var team = CreateTeamAjax ();
-             team.AddPlayer("André","Onana");
-            team.AddPlayer("Matthijs","de Ligt");
-          
-            Assert.Equal(2,team.Players.Count());
+            string response = "";
+            team.AddPlayer ("André", "Onana", out response);
+            team.AddPlayer ("Matthijs", "de Ligt", out response);
+            Assert.Equal (2, team.Players.Count ());
         }
+
         [Fact]
         public void TeamPreventsDuplicatePlayer () {
             var team = CreateTeamAjax ();
-             team.AddPlayer("André","Onana");
-            team.AddPlayer("André","Onana");
-          
-            Assert.Equal(1,team.Players.Count());
+            string response;
+            team.AddPlayer ("André", "Onana", out response);
+            team.AddPlayer ("André", "Onana", out response);
+            Assert.Equal (1, team.Players.Count ());
         }
+
         [Fact]
-        public void CanChangeManager()
-        {
+        public void TeamReturnsDuplicateMessageForDuplicatePlayer () {
             var team = CreateTeamAjax ();
-            team.ChangeManagement(new Manager("Erik", "ten Hag"));
-            Assert.Equal("Erik ten Hag",team.Manager.Name.FullName);
-
+            string response;
+            team.AddPlayer ("André", "Onana", out response);
+            team.AddPlayer ("André", "Onana", out response);
+            Assert.Equal ("Duplicate player", response);
         }
 
+        [Fact]
+        public void CanChangeManager () {
+            var team = CreateTeamAjax ();
+            team.ChangeManagement (new Manager ("Erik", "ten Hag"));
+            Assert.Equal ("Erik ten Hag", team.Manager.Name.FullName);
+        }
+
+        [Fact]
+        public void CanSetHomeColorsWhenAwayColorsAreNotSet () {
+            var team = CreateTeamAjax ();
+            team.SpecifyHomeUniformColors (Color.White, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White, false);
+            Assert.Equal (Color.White, team.HomeColors.ShirtPrimary);
+        }
+
+        [Fact]
+        public void CanSetHomeColorsWhenAwayColorsAreDifferent () {
+            var team = CreateTeamAjax ();
+            team.SpecifyAwayUniformColors (Color.Blue, Color.Red, Color.Empty, Color.Blue, Color.Empty, Color.Blue, false);
+            team.SpecifyHomeUniformColors (Color.White, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White, false);
+            Assert.Equal (Color.White, team.HomeColors.ShirtPrimary);
+        }
+          [Fact]
+        public void CannotSetHomeColorsWhenAwayColorsAreSameAndNotForced () {
+            var team = CreateTeamAjax ();
+            team.SpecifyAwayUniformColors (Color.White, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White, false);
+           var response=team.SpecifyHomeUniformColors (Color.White, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White, false);
+            Assert.Equal (response,false);
+        }
     }
 }
