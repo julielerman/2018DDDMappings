f892a17658af34f609dcc0fa2328a2e63a9882bf
julie <julielerman@gmail.com>
2018-01-27 14:39:21 -0500

    revise player

--- domain/Player.cs
+++ domain/Player.cs
@@ -1,14 +1,14 @@
 using System;
 using SharedKernel;
 
-namespace Domain {
-  public class Player {
-    public Player (string firstname, string lastname) {
-      Name = PersonFullName.Create (firstname, lastname);
-      Id = Guid.NewGuid ();
-    }
-    private Player(){}
-    public Guid Id { get; set; }
-    public PersonFullName Name { get; set; }
+public class Player
+{
+  public Player(string firstname, string lastname)
+  {
+    NameFactory=PersonFullName.Create(firstname,lastname);
+      Id=Guid.NewGuid();
   }
+  public Guid Id { get; private set; }
+  public PersonFullName NameFactory { get; private set; }
+  public string Name=>NameFactory.FullName;
 }
