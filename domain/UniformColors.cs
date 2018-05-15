ffa75fc7d29c4b936f952d5131e67c35a0ae5941
Julie Lerman <julielerman@gmail.com>
2018-05-14 21:26:53 -0400

    colors to system.color

--- domain/UniformColors.cs
+++ domain/UniformColors.cs
@@ -1,31 +1,32 @@
 using System.Drawing;
 using JimmyBogardRocks;
+
 namespace Domain {
   public class UniformColors : ValueObject<UniformColors> {
     public UniformColors (Color shirt1, Color shirt2, Color shirt3, Color shorts1, Color shorts2, Color socks) {
-      ShirtPrimary = shirt1.Name;
-      ShortsSecondary = shirt2.Name;
-      ShirtTertiary = shirt3.Name;
-      ShortsPrimary = shorts1.Name;
-      ShortsSecondary = shorts2.Name;
-      Socks = socks.Name;
+      ShirtPrimary = shirt1;
+      ShortsSecondary = shirt2;
+      ShirtTertiary = shirt3;
+      ShortsPrimary = shorts1;
+      ShortsSecondary = shorts2;
+      Socks = socks;
 
     }
-    public string ShirtPrimary { get; private set; }
-    public string ShirtSecondary { get; private set; }
-    public string ShirtTertiary { get; private set; }
-    public string ShortsPrimary { get; private set; }
-    public string ShortsSecondary { get; private set; }
-    public string Socks { get; private set; }
+    public Color ShirtPrimary { get; private set; }
+    public Color ShirtSecondary { get; private set; }
+    public Color ShirtTertiary { get; private set; }
+    public Color ShortsPrimary { get; private set; }
+    public Color ShortsSecondary { get; private set; }
+    public Color Socks { get; private set; }
 
     public UniformColors RevisedColors (Color shirt1, Color shirt2, Color shirt3, Color shorts1, Color shorts2, Color socks) {
       var newUniformColors = new UniformColors (
-        (shirt1 != null) ? shirt1 : Color.FromName(ShirtPrimary),
-        (shirt2 != null) ? shirt2 : Color.FromName(ShirtSecondary),
-        (shirt3 != null) ? shirt3 : Color.FromName(ShirtTertiary),
-        (shorts1 != null) ? shorts1 : Color.FromName(ShortsPrimary),
-        (shorts2 != null) ? shorts2 : Color.FromName(ShortsSecondary),
-        (socks != null) ? socks : Color.FromName(Socks)
+        (shirt1 != null) ? shirt1 : ShirtPrimary,
+        (shirt2 != null) ? shirt2 : ShirtSecondary,
+        (shirt3 != null) ? shirt3 : ShirtTertiary,
+        (shorts1 != null) ? shorts1 : ShortsPrimary,
+        (shorts2 != null) ? shorts2 : ShortsSecondary,
+        (socks != null) ? socks : Socks
       );
       return newUniformColors;
     }
