e0323c6afb3241aa7ed5d7cbec666a00a9ed7386
julie <julielerman@gmail.com>
2018-11-09 14:25:49 -0500

    notes edited

--- domain/team.cs
+++ domain/team.cs
@@ -6,11 +6,10 @@ using SharedKernel;
 
 namespace Domain {
   public class Team {
-      //private Team(){ } //<==no longer needed in 2.1 yay
+      //private Team(){ } //<==not always needed in 2.1 yay
       public Team (string teamName, string nickname, string yearFounded, string homeStadium) {
       //TeamName = teamName;
       _teamname=teamName;
-      
       Nickname = nickname;
       YearFounded = yearFounded;
       HomeStadium = homeStadium;
@@ -19,14 +18,14 @@ namespace Domain {
     }
     public Guid Id { get; private set; }
     
-    //changed team name so it can only be set
-    //in constructor and never edited
-    //uses backing field  _teamname
+    // team name can only be set
+    // in constructor and never edited
+    // uses backing field  _teamname
     
     //public string TeamName { get; private set; }
     private string _teamname;
     public string TeamName => _teamname;
-                          //{ get; private set; }   
+                           
     public string Nickname { get; private set; }
     public string YearFounded { get; private set; }
     public string HomeStadium { get; private set; } 
