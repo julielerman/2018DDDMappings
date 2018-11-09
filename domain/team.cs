ee1084e60fb56eb87fce06d12904f680882b4c08
julie <julielerman@gmail.com>
2018-11-09 14:28:48 -0500

    rewinding to starting point

--- domain/team.cs
+++ domain/team.cs
@@ -8,8 +8,7 @@ namespace Domain {
   public class Team {
       //private Team(){ } //<==not always needed in 2.1 yay
       public Team (string teamName, string nickname, string yearFounded, string homeStadium) {
-      //TeamName = teamName;
-      _teamname=teamName;
+      TeamName = teamName;
       Nickname = nickname;
       YearFounded = yearFounded;
       HomeStadium = homeStadium;
@@ -17,15 +16,8 @@ namespace Domain {
       Players = new List<Player> ();
     }
     public Guid Id { get; private set; }
-    
-    // team name can only be set
-    // in constructor and never edited
-    // uses backing field  _teamname
-    
-    //public string TeamName { get; private set; }
-    private string _teamname;
-    public string TeamName => _teamname;
-                           
+      
+    public string TeamName { get; private set; }                        
     public string Nickname { get; private set; }
     public string YearFounded { get; private set; }
     public string HomeStadium { get; private set; } 
